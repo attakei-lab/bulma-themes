7f8639bcf7b49d86503a7080dd092b169863f91f
Kazuya Takei <myself@attakei.net>
2026-06-17 00:32:45 +0900

    feat(website): apply simple layout for Bulma

--- apps/website/eleventy.config.ts
+++ apps/website/eleventy.config.ts
@@ -1,7 +1,16 @@
+import type { EleventyConfig } from "@11ty/eleventy";
+
 export const config = {
   dir: {
     input: "./src",
     includes: "_includes",
     layouts: "_layouts",
   },
+  htmlTemplateEngine: "njk",
 };
+
+export default async function (eleventyConfig: EleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    "./assets/": "assets",
+  });
+}
