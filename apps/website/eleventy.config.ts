4cb30a4d5c553a8e1a809bb5f8699aee36ffdf47
Kazuya Takei <myself@attakei.net>
2026-06-17 01:39:45 +0900

    feat(website): add icon link on navbar

--- apps/website/eleventy.config.ts
+++ apps/website/eleventy.config.ts
@@ -1,4 +1,5 @@
 import type { EleventyConfig } from "@11ty/eleventy";
+import fontAwesomePlugin from "@11ty/font-awesome";
 
 export const config = {
   dir: {
@@ -10,6 +11,7 @@ export const config = {
 };
 
 export default async function (eleventyConfig: EleventyConfig) {
+  eleventyConfig.addPlugin(fontAwesomePlugin);
   eleventyConfig.addPassthroughCopy({
     "./assets/": "assets",
   });
